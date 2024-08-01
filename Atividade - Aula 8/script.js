76fab3ef17432f22cd4dcbd78846b3b622bf6fe2
DiegoBorges26 <diego.fort2011@hotmail.com>
2024-08-01 12:04:47 -0300

    Add files via upload

---
+++ Atividade - Aula 8/script.js
@@ -0,0 +1,41 @@
+//Exemplo - 1
+// let paragrafo = document.getElementsByName('nomeQualquer');
+
+// paragrafo[0].innerText = ' test'
+// paragrafo[1].innerText = ' teste2 '
+//------------------------------------------------------------
+
+// Exemplo - 2
+// let paragrafo = document.getElementsByClassName('minhaClass')
+
+// paragrafo[0].innerText = ' escreveno no html'
+//-------------------------------------------------------------
+
+// Exemplo - 3
+// let paragrafo = document.getElementById('texto');
+
+//-------------------------------------------------------------
+
+// Exemplo - 4
+// let h1 = document.querySelector('h1');
+
+// let paragrafo = document.querySelector('#paragrafo');
+
+// let texto = document.querySelector('.texto');
+
+//-------------------------------------------------------------
+
+// Exemplo - 5
+// let paragrafos = document.querySelectorAll('.minhaClasse');
+
+//-------------------------------------------------------------
+
+// Atividade - 1
+
+// function getValue(){
+//     let input = document.querySelector('input').value
+//     console.log( `O valor é ${input}`);
+
+// }
+
+//------------------------------------------------------------
